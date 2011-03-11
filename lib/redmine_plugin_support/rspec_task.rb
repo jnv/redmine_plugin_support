8fa8c1992a80e598bcbfe1de7d23c4266fa87dce
Steffen Roegner <steffen@sroegner.org>
2011-03-11 09:01:34 -0500

    Moving to use rspec2 - no backwards compatibility

--- lib/redmine_plugin_support/rspec_task.rb
+++ lib/redmine_plugin_support/rspec_task.rb
@@ -1,50 +1,47 @@
 gem 'rspec'
 gem 'rspec-rails'
-require 'spec/rake/spectask'
+require "rspec/core/rake_task"
 
 module RedminePluginSupport
   class RspecTask < GeneralTask
     def define
 
       desc "Run all specs in spec directory (excluding plugin specs)"
-      Spec::Rake::SpecTask.new(:spec) do |t|
-        t.spec_opts = ['--options', "\"#{RedmineHelper.plugin_root}/spec/spec.opts\""]
-        t.spec_files = FileList['spec/**/*_spec.rb']
+      RSpec::Core::RakeTask.new(:spec) do |task|
+        task.rspec_opts = ['--options', "\"#{RedmineHelper.plugin_root}/spec/spec.opts\""]
+        task.pattern = 'spec/**/*_spec.rb'
       end
       
       namespace :spec do
         desc "Run all specs in spec directory with RCov (excluding plugin specs)"
-        Spec::Rake::SpecTask.new(:rcov) do |t|
-          t.spec_opts = ['--options', "\"#{RedmineHelper.plugin_root}/spec/spec.opts\""]
-          t.spec_files = FileList['spec/**/*_spec.rb']
-          t.rcov = true
-          t.rcov_opts << ["--rails", "--sort=coverage", "--exclude '/var/lib/gems,spec,#{RedmineHelper.redmine_app},#{RedmineHelper.redmine_lib}'"]
+        RSpec::Core::RakeTask.new(:rcov) do |task|
+          task.rspec_opts = ['--options', "\"#{RedmineHelper.plugin_root}/spec/spec.opts\""]
+          task.pattern = 'spec/**/*_spec.rb'
+          #task.rcov = true
+          #task.rcov_opts << ["--rails", "--sort=coverage", "--exclude '/var/lib/gems,spec,#{RedmineHelper.redmine_app},#{RedmineHelper.redmine_lib}'"]
         end
         
         desc "Print Specdoc for all specs (excluding plugin specs)"
-        Spec::Rake::SpecTask.new(:doc) do |t|
-          t.spec_opts = ["--format", "specdoc", "--dry-run"]
-          t.spec_files = FileList['spec/**/*_spec.rb']
+        RSpec::Core::RakeTask.new(:doc) do |task|
+          task.rspec_opts = ["--format", "specdoc", "--dry-run"]
+          task.pattern = 'spec/**/*_spec.rb'
         end
 
         desc "Print Specdoc for all specs as HTML (excluding plugin specs)"
-        Spec::Rake::SpecTask.new(:htmldoc) do |t|
-          t.spec_opts = ["--format", "html:doc/rspec_report.html", "--loadby", "mtime"]
-          t.spec_files = FileList['spec/**/*_spec.rb']
+        RSpec::Core::RakeTask.new(:htmldoc) do |task|
+          task.rspec_opts = ["--format", "html:doc/rspec_report.html", "mtime"]
+          task.pattern = 'spec/**/*_spec.rb'
         end
 
         [:models, :controllers, :views, :helpers, :lib].each do |sub|
           desc "Run the specs under spec/#{sub}"
-          Spec::Rake::SpecTask.new(sub) do |t|
-            t.spec_opts = ['--options', "\"#{RedmineHelper.plugin_root}/spec/spec.opts\""]
-            t.spec_files = FileList["spec/#{sub}/**/*_spec.rb"]
+          RSpec::Core::RakeTask.new(sub) do |task|
+            task.pattern = "spec/#{sub}/**/*_spec.rb"
           end
         end
-      end
 
-      
+      end
       self
-    end
-    
+    end    
   end
 end
